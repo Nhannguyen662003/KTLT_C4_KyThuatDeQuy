5bb20f447b2b3ae4814a65d295538aaac6627109
Nhannguyen662003 <nguyenthiennhan662003@gmail.com>
2024-08-11 17:12:07 +0700

    Thiện Nhân : Bài 1_1 Chương 4: Hàm tính các biểu thức 1. S(n) = 1 + 2 + 3 + ... + n

---
+++ Baitap_Chuong4/Baitap_Chuong4/Baitap_Chuong4.cpp
@@ -0,0 +1,99 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+
+// Function declarations
+int sum_recursive(int n);
+int sum_non_recursive(int n);
+double sqrt_sum_recursive(int n);
+double sqrt_sum_non_recursive(int n);
+double fraction_sum_recursive(int n);
+double fraction_sum_non_recursive(int n);
+double odd_fraction_sum(int n);
+int product_sum(int n);
+double product_fraction_sum(int n);
+int square_sum(int n);
+int cumulative_sum(int n);
+double alternating_fraction_sum(int n);
+double factorial_expression_sum(int n);
+double complex_expression_sum(int n);
+
+int main() {
+    int choice, n;
+
+    do {
+        system("cls");
+        printf("Chon mot trong cac truong hop sau:\n");
+        printf("1. S(n) = 1 + 2 + 3 + ... + n\n");
+        printf("2. S(n) = sqrt(2) + sqrt(2 + sqrt(2 + ...)) co n dau can\n");
+        printf("3. S(n) = 1/2 + 2/3 + ... + n/(n+1)\n");
+        printf("4. S(n) = 1 + 1/3 + 1/5 + ... + 1/(2n+1)\n");
+        printf("5. S(n) = 1.2 + 2.3 + 3.4 + ... + n.(n+1)\n");
+        printf("6. S(n) = 1/(1.2.3) + 1/(2.3.4) + ... + 1/(n.(n+1).(n+2))\n");
+        printf("7. S(n) = 1^2 + 2^2 + ... + n^2\n");
+        printf("8. S(n) = 1 + (1+2) + (1+2+3) + ... + (1+2+...+n)\n");
+        printf("9. S(n) = (1+2)/2! + (3+4)/4! + ... + (-1)^(n+1) * ((2n-1)+(2n))/(2n)!\n");
+        printf("10. S(n) = (1.2!)/(2+sqrt(3)) + (2.3!)/(3+sqrt(4)) + ... + n(n+1)! / ((n+1)+sqrt(n+2))\n");
+        printf("11. S(n) = (1+sqrt(1+2)) / (2+sqrt(3!)) + ... + (n+sqrt(n+n+1)) / ((n+1)+sqrt(n+2)!)\n");
+        printf("0. Thoat\n");
+        printf("Lua chon cua ban: ");
+        scanf("%d", &choice);
+
+        if (choice != 0) {
+            printf("Nhap so nguyen duong n: ");
+            scanf("%d", &n);
+        }
+
+        switch (choice) {
+        case 1:
+            printf("Ket qua de quy: %d\n", sum_recursive(n));
+            printf("Ket qua khong de quy: %d\n", sum_non_recursive(n));
+            _getch();
+            break;
+        case 2:           
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:        
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            break;
+        case 11:
+            break;
+        case 0:
+            printf("Thoat chuong trinh.\n");
+            break;
+        default:
+            printf("Lua chon khong hop le.\n");
+            break;
+        }
+    } while (choice != 0);
+
+    return 0;
+}
+
+// Function definitions
+int sum_recursive(int n) {
+    if (n == 1) return 1;
+    return n + sum_recursive(n - 1);
+}
+
+int sum_non_recursive(int n) {
+    return n * (n + 1) / 2;
+}
+
+
+
+
