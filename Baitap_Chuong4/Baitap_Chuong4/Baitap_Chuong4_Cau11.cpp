15198ca55e4b0703e7d5130c5539c630620b0de3
nghia09231 <nghia09231@gmail.com>
2024-08-11 20:21:31 +0700

    Huỳnh Nghĩa : Bài 11 Chương 4: Viết hàm xuất dãy có n số Fibonacci, biết rằng số Fibonacci là số có dạng : F(n) = { 1, với n <= 2, { F(n-1) + F(n-2)

---
+++ Baitap_Chuong4/Baitap_Chuong4/Baitap_Chuong4_Cau11.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Hàm tính số Fibonacci F(n) với đệ quy
+int fibonacci(int n) {
+    if (n <= 2) {
+        return 1; // F(n) = 1 với n <= 2
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2); // F(n) = F(n-1) + F(n-2)
+    }
+}
+
+// Hàm xuất dãy Fibonacci có n số
+void printFibonacciSequence(int n) {
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", fibonacci(i));
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+
+    printf("Nhap so luong so trong day Fibonacci: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("So luong phan tu phai la so nguyen duong.\n");
+        return 1;
+    }
+
+    printf("Day Fibonacci gom %d so la:\n", n);
+    printFibonacciSequence(n);
+
+    return 0;
+}
