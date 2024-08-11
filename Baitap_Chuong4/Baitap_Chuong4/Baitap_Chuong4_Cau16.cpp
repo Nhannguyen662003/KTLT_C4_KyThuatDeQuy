5021c44bc05886d7ef49a4865397f66814bbd2f1
nghia09231 <nghia09231@gmail.com>
2024-08-11 21:17:38 +0700

    Huỳnh Nghĩa : Bài 16_1 Chương 4: Với mỗi n>= 1, số Xn được tính như sau: X1=1; X2=1; Xn = Xn-1 + (n-1)Xn-2 với n >= 3. Viết hàm tính Xn bằng 2 cách đệ quy và khử đệ quy

---
+++ Baitap_Chuong4/Baitap_Chuong4/Baitap_Chuong4_Cau16.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// Hàm tính Xn bằng phương pháp đệ quy
+int recursiveXn(int n) {
+    if (n == 1 || n == 2) {
+        return 1;
+    }
+    return recursiveXn(n - 1) + (n - 1) * recursiveXn(n - 2);
+}
+
+int main() {
+    int n;
+
+    printf("Nhap chi so n: ");
+    scanf("%d", &n);
+
+    if (n < 1) {
+        printf("Chi so n phai la so nguyen duong.\n");
+        return 1;
+    }
+
+    int resultRec = recursiveXn(n);
+
+    printf("Gia tri Xn (de quy) khi n = %d la: %d\n", n, resultRec);
+
+    return 0;
+}
